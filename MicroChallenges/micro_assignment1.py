b053a666d943b13d51f354f7ffa7bdc03ce5326b
Austen-R <56137401+Austen-R@users.noreply.github.com>
2019-10-16 23:38:40 -0400

    Add files via upload

--- MicroChallenges/micro_assignment1.py
+++ MicroChallenges/micro_assignment1.py
@@ -1,54 +1,54 @@
-import ephem
-
 class sat:
-	def __init__(self,line1,line2):
-		self.sat_num = line2[1]
-		self.classification = line1[1][-1]
-		self.international_designator = line1[2]
-		self.epoch = line1[3]
-		self.time_deriv_mean_motion1 = line1[4]
-		self.time_deriv_mean_motion2 = line1[5]
-		self.drag_term = line1[6]
-		self.emphemeris_type = line1[7]
-		self.element_num = line1[8]
-		self.inclination = line2[2]
-		self.right_asc_node = line2[3]
-		self.eccentricity = line2[4]
-		self.argument_pedigree = line2[5]
-		self.mean_anomoly = line2[6]
-		self.neam_motion = line2[7]
-		self.elevation = 0
-		#self.revolution_num_epoch = line2[8]
+    def __init__(self,line1,line2):
+        self.sat_num = line2[1]
+        self.classification = line1[1][-1]
+        self.international_designator = line1[2]
+        self.epoch = line1[3]
+        self.time_deriv_mean_motion1 = line1[4]
+        self.time_deriv_mean_motion2 = line1[5]
+        self.drag_term = line1[6]
+        self.emphemeris_type = line1[7]
+        self.element_num = line1[8]
+        self.inclination = line2[2]
+        self.right_asc_node = line2[3]
+        self.eccentricity = line2[4]
+        self.argument_pedigree = line2[5]
+        self.mean_anomoly = line2[6]
+        self.neam_motion = line2[7]
+        #self.revolution_num_epoch = line2[8]
+        self.apogee = ((((6.67430e-11)*(5.9722e24)*(86400**2))/(4*(3.14159**2)*((float(self.neam_motion))**2)))**(1/3))-6378e3
+        
 
-	def __str__(self,):
-		print('sat num :',self.sat_num)
-		print('clasification', self.classification)
-		print('international designator', self.international_designator)
-		print('epoch', self.epoch)
-		print('time derivative of mean motion 1',self.time_deriv_mean_motion1)
-		print('time derivative of mean motion 2',self.time_deriv_mean_motion2)
-		print('drag term', self.drag_term)
-		print('emphemeris type', self.emphemeris_type)
-		print('element number', self.element_num)
-		print('inclination',self.inclination)
-		print('right ascencion node',self.right_asc_node)
-		print('eccentricity : ',self.eccentricity)
-		print('argument pedigree : ',self.argument_pedigree)
-		print('mean anomoly : ',self.mean_anomoly)
-		print('mean mootion : ', self.neam_motion)
-		print('altitude : ', self.elevaiton)
-    #if the satellite is critically inclined
-		if (abs(self.inclination - 64.3) <= 1):
-      print('The satellite is in a critically inclined orbit.')
-    else:
-      print('The satellite is not in a critically inclined orbit.')
-    #if the satellite is in a Molniya orbit
-    if (abs(self.argument_pedigree - 270) <= 20 and abs(self.inclination - 64.3) <= 1):
-      print('The satellite is in a Molyniya orbit.')
-    else:
-      print('The satellite is not in a Molniya orbit.')
-		return ''
-		#print('revolution of number epoch : ',self.revolution_num_epoch)
+    def __str__(self,):
+        print('Satellite catalog number:',self.sat_num)
+        #print('clasification', self.classification)
+        #print('international designator', self.international_designator)
+        #print('epoch', self.epoch)
+        #print('time derivative of mean motion 1',self.time_deriv_mean_motion1)
+        #print('time derivative of mean motion 2',self.time_deriv_mean_motion2)
+        #print('drag term', self.drag_term)
+        #print('emphemeris type', self.emphemeris_type)
+        #print('element number', self.element_num)
+        print('Eccentricity (e): 0.' + self.eccentricity)
+        print('Mean anomaly (M): ',self.mean_anomoly + '˚')
+        print('Right ascension of the ascending node (Ω):',self.right_asc_node + '˚')
+        print('Argument of periapsis (ω): ',self.argument_pedigree + '˚')
+        print('Inclination (i):', self.inclination + '˚')
+        print('Average distance (a):', self.apogee )
+        print('mean mootion : ', self.neam_motion)
+        
+        if self.apogee >= 35800e3:
+            print('Orbital: HEO')
+        elif self.apogee < 35800e3 and self.apogee > 2000e3:
+            if self.apogee < 35886e3 and self.apogee > 35686e3:
+                print('Orbital: GEO')
+            else:  
+                print('Orbital: MEO')
+        else:
+            print('Orbital: LEO')
+            
+        return ''
+        #print('revolution of number epoch : ',self.revolution_num_epoch)
 
 
 #Import the text file:
@@ -60,32 +60,28 @@ tle_list = []
 
 #makes tle_list into a list of lists, made of individual lines with component parts inside them
 for item in tle_str:
-	ind_tle = item.split(' ')
+    ind_tle = item.split(' ')
 
-	#gets rid of the \n term
-	hold = ind_tle[-1].split('\n')
-	ind_tle.pop()
-	for item in hold:
-		ind_tle.append(item)
-	while '' in ind_tle:
-		ind_tle.remove('')
-	tle_list.append(ind_tle)
+    #gets rid of the \n term
+    hold = ind_tle[-1].split('\n')
+    ind_tle.pop()
+    for item in hold:
+        ind_tle.append(item)
+    while '' in ind_tle:
+        ind_tle.remove('')
+    tle_list.append(ind_tle)
 
 def create_satlist(L):
-	final = set()
-	for step in range(0,len(tle_list)-1,2):
-		new = sat(L[step],L[step+1])
-		print(tle_str[step],tle_str[step+1])
-		elea = ephem.readtle('something',tle_str[step],tle_str[step+1])
-		elea.compute()
-		ele = elea.elevation
-		new.elevation = ele
-		final.add(new)
-	return final
+    final = set()
+    for step in range(0,len(tle_list)-1,2):
+        new = sat(L[step],L[step+1])
+        final.add(new)
+    return final
+
 
 satellites = create_satlist(tle_list)
 for item in satellites:
-	print(item)
+    print(item)
 
 
 
